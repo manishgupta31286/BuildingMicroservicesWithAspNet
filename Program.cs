1aceaf346da6e3bfa71bfde34ca31defbaf25b68
manishgupta31286 <manish.gupta.elex@gmail.com>
2022-01-04 21:19:39 +0100

    commit 1

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+            .AddCommandLine(args)
+            .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseConfiguration(config)
+                .Build();
+            host.Run();
+        }
+
+        // public static void Main(string[] args)
+        // {
+        //     BuildWebHost(args).Run();
+        // }
+
+        // public static IWebHost BuildWebHost(string[] args) =>
+        //     WebHostBuilder.crCreateDefaultBuilder(args)
+        //         .UseStartup<Startup>()
+        //         .Build();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace HelloWorld
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        { }
+
+        public void Configure(IApplicationBuilder app,
+        IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello, world!");
+            });
+        }
+    }
+}
